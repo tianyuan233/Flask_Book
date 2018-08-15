16a2f8cf53cfd43974e3f0cf09db8ccd371cbff9
tianyuan233 <1406282384@qq.com>
2018-08-15 23:47:42 +0800

    ？传递参数

--- app/web/book.py
+++ app/web/book.py
@@ -1,18 +1,20 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from exchange_book import ExangeBook
 from helper import is_isbn_or_key
 from . import web
 
 
-@web.route('/book/search/<q>')
-def search(q):
+@web.route('/book/search')
+def search():
     """
 
     :param q: 关键字或者 isbn
     :param page:
     :return:
+    ?q=参数
     """
+    q = request.args['q']
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = ExangeBook.search_by_isbn(q)
