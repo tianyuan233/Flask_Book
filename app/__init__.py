52d451aaa38330bedc87871fdfd182e203cca8d1
tianyuan233 <1406282384@qq.com>
2018-09-04 22:38:30 +0800

    处理404页面

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
-
+from flask_mail import Mail
 from app.models.book import db
 
 login_manager = LoginManager()
-
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +18,8 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'web.login'
     login_manager.login_message = '请先登录或注册'
+
+    mail.init_app(app)
 
     db.create_all(app=app)
 
---
+++ app/libs/email.py
@@ -0,0 +1,5 @@
+from app import mail
+from flask_mail import Message
+
+def send_email(to, subject, template, **kwargs):
+    pass
