34a338d9f3c042a3340379a276b41e2582731d0b
tianyuan233 <1406282384@qq.com>
2018-09-04 23:05:58 +0800

    发邮件

--- app/libs/email.py
+++ app/libs/email.py
@@ -1,5 +1,8 @@
 from app import mail
 from flask_mail import Message
+#to, subject, template, **kwargs
+def send_email():
+    msg = Message('测试',sender='1406282384@qq.com',body='Tets',
+                  recipients=['17691076523@163.com'])
+    mail.send(msg)
 
-def send_email(to, subject, template, **kwargs):
-    pass
